5e4ec80f85a8ff3279a66d3946d83abbfd7d04a5
Jaysoni14-3 <jaysoni998.js@gmail.com>
2023-10-15 00:09:59 +0530

    changes for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    base: "/shreeji/",
+    plugins: [react()],
+  };
+
+  if (command !== "serve") {
+    config.base = "/react-vite-gh-pages/";
+  }
+
+  return config;
 });
