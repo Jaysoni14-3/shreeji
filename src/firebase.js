d0b674dd5c64b8a3f9132ef8b1fc76a5ab6a805f
Jaysoni14-3 <jaysoni998.js@gmail.com>
2023-10-31 12:34:46 +0530

    Firebase config settings

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,21 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+// };
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBX9jWLe3dS4YSrEfjlolyjjNVAKyRszJY",
+  authDomain: "shreeji-a8067.firebaseapp.com",
+  projectId: "shreeji-a8067",
+  storageBucket: "shreeji-a8067.appspot.com",
+  messagingSenderId: "384882244096",
+  appId: "1:384882244096:web:3b734ce985c434a4ffb6e5",
 };
 
 const app = initializeApp(firebaseConfig);
